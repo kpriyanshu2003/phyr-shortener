55e05b75c42cc0ee3590357ad11ddaa7946dbf2d
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-04-21 01:57:20 +0530

    Add new icons to the icons.js file

--- app/page.js
+++ app/page.js
@@ -1,7 +1,25 @@
-import { CommandIcon, IconRight } from "@/static/icons";
-import { Button, Kbd } from "@nextui-org/react";
+"use client";
+
+import {
+  CommandIcon,
+  CustomizeIcon,
+  EyeClosed,
+  EyeOpen,
+  IconRight,
+  Keyicon,
+  LockIcon,
+  StarIcon,
+  UpIcon,
+} from "@/static/icons";
+import { Button, Kbd, Spacer, Switch } from "@nextui-org/react";
+import { useState } from "react";
 
 export default function Home() {
+  const [open, setOpen] = useState(false);
+  const [customUrl, setCustomUrl] = useState("");
+  const [isPassEnabled, setIsPassEnabled] = useState(false);
+  const [isPassVisible, setIsPassVisible] = useState(false);
+
   return (
     <div>
       <nav className="h-20">
@@ -44,6 +62,138 @@ export default function Home() {
             <span className="ml-3">
               <IconRight />
             </span>
+          </div>
+        </div>
+      </div>
+
+      <Spacer y={100} />
+
+      <div className="w-full h-[400px]">
+        <div className="max-w-3xl mx-auto h-full">
+          <h2 className="text-4xl text-center font-semibold">
+            Shorten. Share. Analyze.
+          </h2>
+          <p className="text-center mt-4 text-gray-500 text-sm">
+            Shorten your long URLs, share them with your friends, and analyze
+            them for free.
+          </p>
+
+          <div className="w-full mx-auto max-w-xl bg-stone-900 shadow-xl shadow-neutral-300 rounded-xl mt-8">
+            <div className="px-5">
+              <input
+                className="bg-transparent mt-5 resize-none text-neutral-200 placeholder:text-neutral-500 w-full outline-none text-sm"
+                onChange={(e) => {
+                  if (e.target.value.length > 0) {
+                    setOpen(true);
+                  } else {
+                    setOpen(false);
+                  }
+                }}
+                type="text"
+                placeholder="paste your link here, e.g. https://example.com"
+                name=""
+                id=""
+              />
+
+              <div className="flex items-center justify-between mt-8 pb-5">
+                <Button
+                  onClick={() => setOpen(!open)}
+                  className={`${
+                    open
+                      ? "bg-stone-700 text-stone-200"
+                      : "bg-stone-950 text-stone-100"
+                  }`}
+                  isIconOnly
+                >
+                  <CustomizeIcon />
+                </Button>
+                <Button className="bg-stone-100 text-stone-900">
+                  <div className="flex space-x-1">
+                    <span className="">
+                      <UpIcon />
+                    </span>
+                    <span>Shorten</span>
+                  </div>
+                </Button>
+              </div>
+
+              <div
+                style={{
+                  width: "100%",
+                  height: open ? "170px" : "0px",
+                }}
+                className="transition-all duration-700 w-full mb-5 rounded-lg overflow-hidden"
+              >
+                <div className="h-[90%] w-full bg-neutral-700/20 rounded-lg p-5">
+                  <div className="text-sm text-neutral-400 flex items-center justify-between">
+                    <div className="w-fit flex items-center space-x-2">
+                      <StarIcon />
+                      <p>Custom branding</p>
+                    </div>
+                    <div className="flex items-center text-right">
+                      <p>sh.phyr.in /</p>
+                      <input
+                        placeholder="alias"
+                        id="custom-url"
+                        size={
+                          customUrl.length > 0
+                            ? customUrl.length > 20
+                              ? 20
+                              : customUrl.length
+                            : 4
+                        }
+                        onChange={(e) => {
+                          setCustomUrl(e.target.value);
+                        }}
+                        className="bg-transparent max-w-sm ml-2 text-neutral-200 text-left w-fit placeholder:text-neutral-500 outline-none text-sm"
+                        type="text"
+                        name=""
+                      />
+                    </div>
+                  </div>
+                  <div className="text-sm text-neutral-400 flex items-center justify-between mt-5">
+                    <div className="w-fit flex items-center space-x-2">
+                      <LockIcon />
+                      <p>Password potected</p>
+                    </div>
+                    <div className="flex items-center text-right dark">
+                      <Switch
+                        value={isPassEnabled}
+                        onChange={() => setIsPassEnabled(!isPassEnabled)}
+                        size="sm"
+                      />
+                    </div>
+                  </div>
+                  <div
+                    style={{
+                      width: "100%",
+                      height: isPassEnabled ? "auto" : "0px",
+                    }}
+                    className="text-sm text-neutral-400 flex items-center justify-between mt-5 overflow-hidden transition-all duration-300"
+                  >
+                    <div className="w-fit flex items-center space-x-2 pl-7">
+                      <p>Set a password</p>
+                    </div>
+                    <div className="flex items-center text-right dark">
+                      <input
+                        type="text"
+                        placeholder="・・・・・・・"
+                        size={15}
+                        className="px-3 pb-1 rounded-none bg-transparent border-neutral-600 outline-none focus-within:border-neutral-500 text-right border-b transition-all"
+                        name=""
+                        id=""
+                      />
+                      <button
+                        onClick={() => setIsPassVisible(!isPassVisible)}
+                        className="ml-3 text-neutral-100"
+                      >
+                        {isPassVisible ? <EyeOpen /> : <EyeClosed />}
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- static/icons.js
+++ static/icons.js
@@ -32,3 +32,142 @@ export const IconRight = () => {
     </svg>
   );
 };
+
+export const CustomizeIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+    >
+      <g fill="none" stroke="currentColor" stroke-linecap="round">
+        <path d="M10 8h10M4 16h10" />
+        <circle cx="7" cy="8" r="3" transform="rotate(90 7 8)" />
+        <circle cx="17" cy="16" r="3" transform="rotate(90 17 16)" />
+      </g>
+    </svg>
+  );
+};
+
+export const UpIcon = () => {
+  return (
+    <svg
+      className="rotate-90"
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="currentColor"
+        strokeWidth={2}
+        d="m20 10l.707.707l.707-.707l-.707-.707zM3 18a1 1 0 1 0 2 0zm12.707-2.293l5-5l-1.414-1.414l-5 5zm5-6.414l-5-5l-1.414 1.414l5 5zM20 9H10v2h10zM3 16v2h2v-2zm7-7a7 7 0 0 0-7 7h2a5 5 0 0 1 5-5z"
+      />
+    </svg>
+  );
+};
+
+export const StarIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 28 28"
+    >
+      <path
+        fill="currentColor"
+        d="M10.06 18.701a1.628 1.628 0 0 0 2.43-.676l.77-2.34a3.82 3.82 0 0 1 2.416-2.418l2.238-.727a1.6 1.6 0 0 0 .786-.595a1.62 1.62 0 0 0-.849-2.489l-2.215-.72a3.82 3.82 0 0 1-2.42-2.414l-.727-2.237a1.62 1.62 0 0 0-.594-.785a1.655 1.655 0 0 0-1.879 0a1.63 1.63 0 0 0-.6.8L8.68 6.365a3.82 3.82 0 0 1-2.359 2.37l-2.24.726a1.626 1.626 0 0 0 .02 3.073l2.216.72a3.86 3.86 0 0 1 1.816 1.286c.266.343.471.728.606 1.14l.728 2.234c.112.318.32.593.595.787m-3.273-6.873l-2.22-.72s-.085-.038-.085-.108a.14.14 0 0 1 .084-.115L6.8 10.16a5.34 5.34 0 0 0 3.32-3.375l.721-2.219s.043-.084.114-.084s.113.084.113.084l.723 2.223a5.32 5.32 0 0 0 3.377 3.372l2.27.735a.13.13 0 0 1 .078.11a.15.15 0 0 1-.083.113l-2.225.722a5.32 5.32 0 0 0-3.374 3.374l-.715 2.2a.11.11 0 0 1-.115.093c-.104 0-.118-.075-.118-.075l-.723-2.22a5.32 5.32 0 0 0-3.376-3.385m13.017 12.954A1.2 1.2 0 0 0 20.5 25a1.2 1.2 0 0 0 .692-.216a1.2 1.2 0 0 0 .446-.6l.372-1.143a1.61 1.61 0 0 1 1.017-1.02l1.166-.378A1.21 1.21 0 0 0 25 20.505a1.2 1.2 0 0 0-.844-1.146l-1.144-.37a1.61 1.61 0 0 1-1.02-1.018l-.38-1.163a1.2 1.2 0 0 0-2.274.016l-.374 1.146a1.61 1.61 0 0 1-.993 1.017l-1.166.378a1.21 1.21 0 0 0-.807 1.139a1.2 1.2 0 0 0 .823 1.138l1.144.372a1.6 1.6 0 0 1 1.02 1.023l.379 1.163a1.2 1.2 0 0 0 .44.582m-1.371-4.194l-.27-.088l.283-.096a3.12 3.12 0 0 0 1.943-1.974l.088-.27l.089.272a3.11 3.11 0 0 0 1.974 1.975l.293.1l-.271.087a3.12 3.12 0 0 0-1.974 1.977l-.088.27l-.088-.272a3.12 3.12 0 0 0-1.98-1.98"
+      />
+    </svg>
+  );
+};
+
+export const LockIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+    >
+      <g fill="none" stroke="currentColor" stroke-width="1.55">
+        <path d="M4 13c0-1.886 0-2.828.586-3.414C5.172 9 6.114 9 8 9h8c1.886 0 2.828 0 3.414.586C20 10.172 20 11.114 20 13v2c0 2.828 0 4.243-.879 5.121C18.243 21 16.828 21 14 21h-4c-2.828 0-4.243 0-5.121-.879C4 19.243 4 17.828 4 15z" />
+        <path
+          stroke-linecap="round"
+          d="M16 8V7a4 4 0 0 0-4-4v0a4 4 0 0 0-4 4v1"
+        />
+      </g>
+    </svg>
+  );
+};
+
+export const Keyicon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+    >
+      <g fill="none">
+        <path
+          fill="currentColor"
+          fill-opacity="0.25"
+          d="M3 11c0-3.771 0-5.657 1.172-6.828C5.343 3 7.229 3 11 3h2c3.771 0 5.657 0 6.828 1.172C21 5.343 21 7.229 21 11v2c0 3.771 0 5.657-1.172 6.828C18.657 21 16.771 21 13 21h-2c-3.771 0-5.657 0-6.828-1.172C3 18.657 3 16.771 3 13z"
+        />
+        <circle cx="8.5" cy="11.5" r="2" stroke="currentColor" />
+        <path
+          stroke="currentColor"
+          stroke-linecap="round"
+          d="M10.5 11.5h5m2 2v-1.85a.15.15 0 0 0-.15-.15H15.5m0 0v2"
+        />
+      </g>
+    </svg>
+  );
+};
+
+export const EyeClosed = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+    >
+      <g
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="1"
+      >
+        <path d="M3 13c3.6-8 14.4-8 18 0" />
+        <path d="M12 17a3 3 0 1 1 0-6a3 3 0 0 1 0 6" />
+      </g>
+    </svg>
+  );
+};
+
+export const EyeOpen = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+    >
+      <g
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="1"
+      >
+        <path d="M3 13c3.6-8 14.4-8 18 0" />
+        <path fill="currentColor" d="M12 17a3 3 0 1 1 0-6a3 3 0 0 1 0 6" />
+      </g>
+    </svg>
+  );
+};
