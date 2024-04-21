561d89a91ee4d0179266323de937ceebf014d1dc
Kumar Priyanshu <az.priyanshu.rockstar@gmail.com>
2024-04-21 16:29:19 +0530

    Refactor prisma imports and update link.js and prisma.js files

--- prisma/link.js
+++ prisma/link.js
@@ -1,33 +1,35 @@
-// import { prisma } from "../prisma";
-// import bcrypt from "bcrypt";
+"use server";
 
-// export const createLink = async (formData) => {
-//   try {
-//     const link = await prisma.link.create({
-//       data: {
-//         url: formData.url,
-//         publicId: formData.publicId,
-//         ipAddr: formData.ipAddr,
-//         password: formData.password
-//           ? await bcrypt.hash(formData.password, 10)
-//           : null,
-//       },
-//     });
-//     return { success: true, link };
-//   } catch (e) {
-//     console.error(e);
-//     if (e.code === "P2002")
-//       return { success: false, error: "Public ID already exists" };
-//     return { success: false, error: e.message };
-//   }
-// };
+import bcrypt from "bcrypt";
+import prisma from "./prisma";
 
-// export const getLink = async (publicId) => {
-//   try {
-//     const link = await prisma.link.findUnique({ where: { publicId } });
-//     return { success: true, link };
-//   } catch (e) {
-//     console.error(e);
-//     return { success: false, error: e.message };
-//   }
-// };
+export const createLink = async (formData) => {
+  try {
+    const link = await prisma.link.create({
+      data: {
+        url: formData.url,
+        publicId: formData.publicId,
+        ipAddr: formData.ipAddr,
+        password: formData.password
+          ? await bcrypt.hash(formData.password, 10)
+          : null,
+      },
+    });
+    return { success: true, link };
+  } catch (e) {
+    console.error(e);
+    if (e.code === "P2002")
+      return { success: false, error: "Public ID already exists" };
+    return { success: false, error: e.message };
+  }
+};
+
+export const getLink = async (publicId) => {
+  try {
+    const link = await prisma.link.findUnique({ where: { publicId } });
+    return { success: true, link };
+  } catch (e) {
+    console.error(e);
+    return { success: false, error: e.message };
+  }
+};
