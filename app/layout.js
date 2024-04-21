99eafcbfd9be3a54ed1a7af5a883efe489962ad8
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-04-21 16:50:41 +0530

    Add react-hot-toast for displaying notifications in layout.js and page.js

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Providers } from "./providers";
 import Layout from "@/components/Layout";
 import GlobalState from "@/context/GlobalState";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,23 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Providers>
           <Layout>{children}</Layout>
+          <Toaster
+            position="bottom-right"
+            containerStyle={{
+              bottom: "40px",
+              right: "40px",
+            }}
+            toastOptions={{
+              style: {
+                background: "#111111",
+                color: "#cdcdcd",
+                padding: "10px 15px",
+                fontSize: "14px",
+                borderRadius: "8px",
+                border: "1px solid #1c1c1c",
+              },
+            }}
+          />
         </Providers>
       </body>
     </html>
