026bbd8784ff0a9de56897f391c6a2226dac130b
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-04-22 21:15:09 +0530

    Update loading screen to display spinner and redirecting message

--- app/[pid]/loading.js
+++ app/[pid]/loading.js
@@ -1,7 +1,21 @@
+import { IconRight } from "@/static/icons";
+import { Skeleton, Spinner } from "@nextui-org/react";
 import React from "react";
 
 function Loading() {
-  return <div className="h-svh bg-red-400">loading</div>;
+  return (
+    <div className="h-[50vh] pt-16">
+      <div className="max-w-xl mx-auto w-full flex flex-col justify-center items-center">
+        <div className="text-gray-600">
+          <Spinner color="current" />
+        </div>
+        <h2 className="text-2xl font-semibold mt-6">Redirecting ...</h2>
+        <p className="text-sm text-neutral-600 mt-4">
+          our servers are verifying the request, please wait
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Loading;
