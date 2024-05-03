97cf09293d66ba8f87d944cfeee8717a1804607c
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-05-04 00:31:58 +0530

    Update UI and fix bugs in Advertisement.js and Navbar.js

--- components/Advertisement.js
+++ components/Advertisement.js
@@ -7,15 +7,15 @@ function Advertisement() {
   return (
     <Link href="https://phyr.global" className="w-fit mx-auto">
       <div className="w-fit flex items-center bg-white border rounded-xl border-neutral-200 p-3 shadow-xl shadow-neutral-100/0 transition-all hover:shadow-neutral-200 hover:scale-100">
-        <div className="h-14 w-14 rounded-md bg-lime-300 p-2 shrink-0">
+        <div className="md:h-14 h-12 w-12 md:w-14 rounded-md bg-lime-300 p-2 shrink-0">
           <img src="/phyr-logo.svg" className="h-full w-full" alt="" />
         </div>
         <div className="ml-4">
           <div className="flex items-start justify-between">
-            <h3 className="text-lg font-semibold">Phyr Studios</h3>
+            <h3 className="text-base md:text-lg font-semibold">Phyr Studios</h3>
             <RightTop />
           </div>
-          <p className="text-sm mt-1 text-neutral-600 line-clamp-1">
+          <p className="text-xs md:text-sm mt-1 text-neutral-600 line-clamp-1">
             design, develop, and deploy digital products.
           </p>
         </div>
--- components/Navbar.js
+++ components/Navbar.js
@@ -5,13 +5,17 @@ import { CommandIcon, IconRight, MenuIcon } from "@/static/icons";
 import { Button } from "@nextui-org/react";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import toast from "react-hot-toast";
+import Banner from "./Banner";
+import Advertisement from "./Advertisement";
+import { AnimatePresence, motion } from "framer-motion";
 
 function Navbar() {
   const pathname = usePathname();
   const router = useRouter();
   const { url, setUrl } = useContext(GlobalState);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
   const handlePaste = () => {
     if (pathname == "/") {
       navigator.clipboard
@@ -66,6 +70,7 @@ function Navbar() {
               </div>
             </Button>
             <Button
+              onClick={() => setSidebarOpen(true)}
               radius="full"
               isIconOnly
               className="bg-neutral-100 ml-4 lg:hidden"
@@ -82,6 +87,90 @@ function Navbar() {
           </div>
         </div>
       </nav>
+      <>
+        <AnimatePresence>
+          {sidebarOpen && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              className="z-20 fixed inset-0 h-full w-full bg-gradient-to-t from-black/50 to-black/60"
+            ></motion.div>
+          )}
+          {sidebarOpen && (
+            <motion.div
+              initial={{ y: "-100%" }}
+              animate={{ y: 0 }}
+              exit={{ y: "-100%" }}
+              transition={{ duration: 0.7 }}
+              className="z-20 fixed inset-0 h-full w-full bg-transparent"
+            >
+              <div className="w-full bg-white py-4">
+                <div className="flex items-center justify-end px-4">
+                  <Button
+                    onClick={() => setSidebarOpen(false)}
+                    isIconOnly
+                    className="bg-lime-300"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width={20}
+                      height={20}
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        fill="none"
+                        stroke="currentColor"
+                        strokeDasharray={12}
+                        strokeDashoffset={12}
+                        strokeLinecap="round"
+                        strokeWidth={2}
+                        d="M12 12L19 19M12 12L5 5M12 12L5 19M12 12L19 5"
+                      >
+                        <animate
+                          fill="freeze"
+                          attributeName="stroke-dashoffset"
+                          dur="0.4s"
+                          values="12;0"
+                        ></animate>
+                      </path>
+                    </svg>
+                  </Button>
+                </div>
+                <Link href="/" className="w-fit">
+                  <div className="flex items-center px-6">
+                    <img className="h-12" src="/logo.svg" alt="" />
+                    <h2 className="font-semibold ml-2 text-2xl">Shortener.</h2>
+                  </div>
+                </Link>
+                <p className="text-sm text-neutral-600 px-6 mt-2">
+                  The easiest way to shorten your links.
+                </p>
+                <ul className="space-y-7 mt-12 px-6 text-base">
+                  <li>
+                    <Link href="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link href="https://phyrs-studios.gitbook.io/shortener-by-phyr/shorten-a-new-link">
+                      API Docs
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/#features">Features</Link>
+                  </li>
+                  <li>
+                    <Link href="/h">History</Link>
+                  </li>
+                </ul>
+
+                <div className="px-5 mt-16 mb-7">
+                  <Advertisement />
+                </div>
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </>
     </>
   );
 }
